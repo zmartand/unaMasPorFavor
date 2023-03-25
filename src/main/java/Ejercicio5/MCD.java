c8406753723e12fefad0139c5bed946c757f26ed
Zara Martín <zmartand@myuax.es>
2023-03-25 18:17:25 +0100

    Ejercicio 5 terminado

---
+++ src/main/java/Ejercicio5/MCD.java
@@ -0,0 +1,6 @@
+package Ejercicio5;
+
+@FunctionalInterface
+public interface MCD {
+    int calculate(int m, int n);
+}
--- src/main/java/Ejercicio5/MaximoComunDivisor.java
+++ src/main/java/Ejercicio5/MaximoComunDivisor.java
@@ -1,41 +1,33 @@
 package Ejercicio5;
 
-import java.util.function.IntUnaryOperator;
-
-import java.util.function.BiFunction;
-
 public class MaximoComunDivisor {
-    public static int gcdRecursive(int m, int n) {
-        if (n == 0) {
-            return m;
-        } else {
-            return gcdRecursive(n, m % n);
-        }
-    }
-
-    public static int gcdIterative(int m, int n) {
-        while (n != 0) {
-            int r = m % n;
-            m = n;
-            n = r;
-        }
-        return m;
-    }
-
-    public static BiFunction<Integer, Integer, Integer> gcdLambda = (m, n) -> n == 0 ? m : gcdLambda.apply(n, m % n);
-
     public static void main(String[] args) {
-        int m = 24;
-        int n = 36;
+        int m = 56;
+        int n = 48;
 
-        int gcd1 = gcdRecursive(m, n);
-        int gcd2 = gcdIterative(m, n);
-        int gcd3 = gcdLambda.apply(m, n);
+        MCD mcdIterativo = (a, b) -> {
+            int r;
+            while (b != 0) {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        };
 
-        System.out.println("m = " + m + ", n = " + n);
-        System.out.println("GCD (recursive) = " + gcd1);
-        System.out.println("GCD (iterative) = " + gcd2);
-        System.out.println("GCD (lambda) = " + gcd3);
+        MCD mcdRecursivo = new MCD() {
+            @Override
+            public int calculate(int a, int b) {
+                if (b == 0) {
+                    return a;
+                } else {
+                    return calculate(b, a % b);
+                }
+            }
+        };
+
+        System.out.println("MCD iterativo con expresión lambda: " + mcdIterativo.calculate(m, n));
+        System.out.println("MCD recursivo con interfaz funcional: " + mcdRecursivo.calculate(m, n));
     }
 }
 
